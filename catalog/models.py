039f14607ad726e278ce1ca5385423ae897ce1ea
DiannaaaMm <murlaevadiana@gmail.com>
2025-03-27 16:14:28 +0300

    add some updates

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Name", help_text="Category Name")
+    description = models.TextField(verbose_name="Description", help_text="Description")
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+        ordering = ['-created_at']
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+        def __str__(self):
+            return f'{self.name}'
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Name", help_text="Product Name")
+    price = models.FloatField(verbose_name="Price", help_text="Price")
+    description = models.TextField(verbose_name="Description", help_text="Description")
+    category = models.ForeignKey(Category, verbose_name="Category", help_text="Category", on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/', verbose_name="Image", null=True, blank=True, help_text="Image")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Date Added", help_text="Date Added")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Date Last Modified", help_text="Date Last Modified")
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+        ordering = ['-created_at']
+
+    def __init__(self, name, price, description, category, image, created_at, updated_at):
+        self.name = name
+        self.price = price
+        self.description = description
+        self.category = category
+        self.image = image
+        self.created_at = created_at
+        self.updated_at = updated_at
+
+    def __str__(self):
+        return f'{self.category} === {self.name} -  {str(self.price)}\n {self.description}  \nYour path to image: {self.image} \n{self.created_at} \n{self.updated_at}'
--- config/settings.py
+++ config/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h+=p*phg)5vvc!k2py9kgdnz#c53q1ssac6pngc702x+wr7c7@'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,7 +81,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.getenv("POSTGRES_DB"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+        'HOST': os.getenv("POSTGRES_HOST"),
+        'PORT': os.getenv("POSTGRES_PORT"),
     }
 }
 
